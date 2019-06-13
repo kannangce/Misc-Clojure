d200343775411cc5ea1a1c540cc645601c6f9ff4
Kannan Ramamoorthy <kannangce@rediffmail.com>
2019-06-13 22:01:29 +0530

    Fixed van eck

--- van-eck-generator.clj
+++ van-eck-generator.clj
@@ -1,18 +1,16 @@
-; This contains the inprogress changes for van eck's sequence generator.
-; This is not perfect now. Needs fixes
-
-(defn van-eck-1
+(defn van-eck
   "Generates a lazy sequence of van eck's sequence"
   ([]
-   (van-eck-1 [0 0]))
+   (van-eck []))
   ([curr-seq]
-   (let [next-num (inc ; To calculate the # steps we inc the index
-                    (index-from-last
-                     (butlast curr-seq)
-                     (last curr-seq)))]
+   (let [next-num (if (empty? curr-seq) 0
+                    (inc ; To calculate the # steps we inc the index
+                     (index-from-last
+                      (butlast curr-seq)
+                      (last curr-seq))))]
     (lazy-seq
-      (cons next-num (van-eck-1 (conj curr-seq next-num)))))))
-      
+      (cons next-num (van-eck (conj curr-seq next-num)))))))
+
 (defn index-from-last
   "Finds the index of 'to-find' from the last element in the given sequence 'seq-from'
   If the element is not found, -1 is returned."
@@ -22,7 +20,6 @@
       (let [index (- (dec seq-len) rev-index)]
         (if (= to-find (nth seq-from index))
             rev-index
-            (if (= rev-index (dec seq-len))
+            (if (>= rev-index (dec seq-len))
              -1
              (recur (inc rev-index))))))))
-             
